46007c9d115aeb785fd2ff1c798c97d425a39231
Lipika Baniya <89762072+LipikaBaniya89@users.noreply.github.com>
2022-04-22 23:26:06 +0545

    Added

---
+++ src/components/Pokedex.js
@@ -0,0 +1,232 @@
+import React, { Component } from 'react'
+import '../App.css'
+import Pokemon from './Pokemon';
+import styled from "styled-components";
+import Cute from "../cute.png";
+import { useDispatch } from "react-redux";
+import { FaPlus, FaTrashAlt, FaCross, FaSearch } from 'react-icons/fa';
+
+class Pokedex extends Component {
+    // Constructor 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            openModal:true,
+            DataisLoaded: false,
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:3030/api/cards")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json.cards,
+                    DataisLoaded: true
+                });
+            })
+    }
+
+    addNewPokemon(pokemon) {
+        this.props.addPokemon(pokemon);
+    }
+
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded)
+            return <div>
+                <h1> Pleses wait some time.... </h1>
+            </div>;
+
+        if (!this.props.openModal) {
+            return null;
+        }
+
+        const HP = (HP) => {
+            if (Number(HP) > 100 ) {
+                return "100";
+            } else {
+                return "0";
+            }
+        }
+    
+        const StrengthLevel = (STR) => {
+            if (STR === undefined) {
+              return "0";
+            }
+        
+            STR = STR.length;
+            if (STR === 1) {
+              return "50";
+            } else if (STR === 2) {
+              return "100";
+            } else if (STR === undefined) {
+              return "0";
+            } else {
+              return "0";
+            }
+          };
+    
+          const Weakness = (weak) => {
+            if (weak === undefined) {
+              return "0";
+            }
+            weak = weak.length;
+        
+            if (weak === 1) {
+              return "100";
+            } else {
+              return "0";
+            }
+          };
+    
+          const filter = (damage) => {
+            if (isNaN(damage[damage.length - 1])) {
+                return Number(damage.substring(0, damage.length - 1));
+              } else {
+                return Number(damage);
+              }
+          };
+    
+          const damage = (attacks) => {
+              if (attacks === undefined) {
+                  return 0;
+              }
+              var count = 0;
+              for (var i = 0; i< attacks.length; i++) {
+                  const damage = filter(attacks[i].damage);
+                  count += damage;
+              }
+              return count;
+          }
+    
+          const Happiness = (hp, attacks, weak) => {
+              let Happiness = 
+              (Number(HP(hp) / 10 ) + damage(attacks) / 10 + 10 - Weakness(weak) / 100) /5;
+    
+              return Math.floor(Happiness);
+          };
+
+          const HappenessLogos = [];
+
+          for (
+            let i = 0;
+            i < Happiness(items.hp, items.attacks, items.weaknesses);
+            i += 1
+          ) {
+            HappenessLogos.push(<img src={Cute} alt="cut" />);
+          }
+      
+       
+    return (  
+        <>
+        <div className='CardSize poke-modal'>
+                <div className='searchBar'>
+                    <input type="text" className="searchBar" placeholder="Search pokemon by name"/>
+                    <FaSearch style={{fontSize:"20px", color:"#fafafa"}}/>   
+                </div>
+
+            <div className='modal-content'>
+                <div className='searchList'>
+                    <ul>
+                        {items.map((pokemon) => (
+                            <li className="flex" key={pokemon.id}>
+                                <img className="cardImg" src={pokemon.imageUrl} alt="Card image cap" />
+                                <div className="cardBody">
+                                    <h5 className="cardTitle">{pokemon.name}</h5>
+                                    <div className="pokemondetails">
+                                        <div className="ability">
+                                            <h4>HP</h4>
+                                            <h4>STR</h4>
+                                            <h4>Weak</h4>
+                                        </div>
+
+                                        <div className="values">
+                                            <Hp percentage={HP(pokemon.hp)}>
+                                            <div></div>
+                                            </Hp>
+                                            <Str percentage={StrengthLevel(pokemon.attacks)}>
+                                            <div></div>
+                                            </Str>
+                                            <Weak percentage={Weakness(pokemon.weaknesses)}>
+                                            <div></div>
+                                            </Weak>
+                                        </div>
+                                    </div>
+
+                                    <Happinesslevel>
+                                    <Happinesslevel>{HappenessLogos}</Happinesslevel>
+                                    </Happinesslevel>
+                                </div>
+                                <button className='success'><FaPlus/></button>
+                            </li>
+                        ))
+                        }
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <Pokedex closeModal={this.state.closeModal}/>
+    </>
+        );
+    }  
+}
+
+const Hp = styled.div`
+  width: 10rem;
+  height: 1.8rem;
+  border-radius: 3rem;
+  background-color: #e4e4e4;
+  div {
+    background-color: #f3701a;
+    border-radius: 3rem;
+    width: ${(props) => props.percentage + "%"};
+    height: 100%;
+  }
+
+`;
+const Str = styled.div`
+  width: 10rem;
+  height: 1.8rem;
+  border-radius: 3rem;
+  background-color: #e4e4e4;
+  div {
+    background-color: #f3701a;
+    border-radius: 3rem;
+    width: ${(props) => props.percentage + "%"};
+    height: 100%;
+  }
+  
+`;
+const Weak = styled.div`
+  width: 10rem;
+  height: 1.8rem;
+  border-radius: 3rem;
+  background-color: #e4e4e4;
+
+  div {
+    background-color: #f3701a;
+    border-radius: 3rem;
+    width: ${(props) => props.percentage + "%"};
+    height: 100%;
+  }
+`;
+
+const Happinesslevel = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 2.5rem;
+  height: 2.5rem;
+  img {
+    margin: 0.1rem;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  
+`;
+
+export default Pokedex
---
+++ src/components/Pokemon.js
@@ -0,0 +1,264 @@
+import React, { Component } from 'react'
+// Make custom stuff. Remove these
+import '../App.css'
+import Card from '../img/card.jpg'
+import { FaPlus, FaTrashAlt, FaCross } from 'react-icons/fa';
+import Pokedex from "./Pokedex";
+import styled from "styled-components";
+import Cute from "../cute.png";
+import ReactDOM from "react-dom";
+
+class Pokemon extends Component {
+    // Constructor 
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            openModal: false,
+        };
+    }
+
+    showModal = e => {
+        this.setState({
+            // Make this toggle. Set value opposite of current value
+            openModal: true
+        });
+    };
+
+    handleClick = () => {
+        if (!this.state.openModal) {
+          document.addEventListener("click", this.handleOutsideClick, false);
+        } else {
+          document.removeEventListener("click", this.handleOutsideClick, false);
+        }
+    
+        this.setState(prevState => ({
+          openModal: !prevState.openModal
+        }));
+      };
+    
+      handleOutsideClick = e => {
+        if (!this.node.contains(e.target)) this.handleClick();
+      };
+
+      hasPokemonInList(id) {
+        return this.state.myPokeList.some(element => {
+            if (element.id === id) {
+                return true;
+            }
+        });
+    }
+
+    addPokemonToList = e => {
+        const isFound = this.hasPokemonInList(e.id)
+        
+        if(!isFound) {
+            this.setState({
+                myPokeList: [...this.state.myPokeList, e]
+            })
+        } else {
+            alert("Pokemon already added")
+        }
+    }
+
+    removePokemon(pokeID) {
+        const updatedList = this.state.myPokeList.filter(item => item.id !== pokeID);
+        this.setState({
+            myPokeList: updatedList
+        })
+    }
+
+render() {
+    const { myPokeList } = this.state;
+    const {items } = this.state;
+
+    const HP = (HP) => {
+        if (Number(HP) > 100 ) {
+            return "100";
+        } else {
+            return "0";
+        }
+    }
+
+    const StrengthLevel = (STR) => {
+        if (STR === undefined) {
+          return "0";
+        }
+    
+        STR = STR.length;
+        if (STR === 1) {
+          return "50";
+        } else if (STR === 2) {
+          return "100";
+        } else if (STR === undefined) {
+          return "0";
+        } else {
+          return "0";
+        }
+      };
+
+      const Weakness = (weak) => {
+        if (weak === undefined) {
+          return "0";
+        }
+        weak = weak.length;
+    
+        if (weak === 1) {
+          return "100";
+        } else {
+          return "0";
+        }
+      };
+
+      const filter = (damage) => {
+        if (isNaN(damage[damage.length - 1])) {
+            return Number(damage.substring(0, damage.length - 1));
+          } else {
+            return Number(damage);
+          }
+      };
+
+      const damage = (attacks) => {
+          if (attacks === undefined) {
+              return 0;
+          }
+          var count = 0;
+          for (var i = 0; i< attacks.length; i++) {
+              const damage = filter(attacks[i].damage);
+              count += damage;
+          }
+          return count;
+      }
+
+      const Happiness = (hp, attacks, weak) => {
+          let Happiness = 
+          (Number(HP(hp) / 10 ) + damage(attacks) / 10 + 10 - Weakness(weak) / 100) /5;
+
+          return Math.floor(Happiness);
+      };
+
+      const HappenessLogos = [];
+
+      for (
+        let i = 0;
+        i < Happiness(items.hp, items.attacks, items.weaknesses);
+        i += 1
+      ) {
+        HappenessLogos.push(<img src={Cute} alt="cut" />);
+      }
+  
+    return (
+            <>
+             <div ref={node => { this.node = node; }}></div>
+                <h1 className='header'> Pokedex </h1>
+                <p className='para'>Is your pokemon on our Codex?</p>
+                <div className="App-logo">
+                        <button className="add" onClick={e => {this.showModal();}}> <FaPlus/> Add</button>
+                        {this.state.openModal && (
+                            <button onClick={() => this.handleClick()}>close modal</button>
+                        )}     
+                </div>
+
+                <div className='modal-content'>
+                    <div className='searchList'>
+                        <ul>
+                            {items.map((pokemon) => (
+                                <li className="flex" key={pokemon.id}>
+                                    <img className="cardImg" src={pokemon.imageUrl} alt="Card image cap" />
+                                    <div className="cardBody">
+                                        <h5 className="cardTitle">{pokemon.name}</h5>
+                                        <div className="pokemondetails">
+                                            <div className="ability">
+                                                <h4>HP</h4>
+                                                <h4>STR</h4>
+                                                <h4>Weak</h4>
+                                            </div>
+
+                                            <div className="values">
+                                                <Hp percentage={HP(pokemon.hp)}>
+                                                <div></div>
+                                                </Hp>
+                                                <Str percentage={StrengthLevel(pokemon.attacks)}>
+                                                <div></div>
+                                                </Str>
+                                                <Weak percentage={Weakness(pokemon.weaknesses)}>
+                                                <div></div>
+                                                </Weak>
+                                            </div>
+                                    </div>
+
+                                    <Happinesslevel>
+                                    <Happinesslevel>{HappenessLogos}</Happinesslevel>
+                                    </Happinesslevel>
+                                </div>
+                                <button className='success'><FaPlus/></button>
+                            </li>
+                        ))
+                        }
+                    </ul>
+                </div>
+            </div>
+  
+                <Pokedex openModal={this.state.openModal} />
+        
+            </>
+        );
+    }
+}
+const Hp = styled.div`
+  width: 10rem;
+  height: 1.8rem;
+  border-radius: 3rem;
+  background-color: #e4e4e4;
+  div {
+    background-color: #f3701a;
+    border-radius: 3rem;
+    width: ${(props) => props.percentage + "%"};
+    height: 100%;
+  }
+
+`;
+const Str = styled.div`
+  width: 10rem;
+  height: 1.8rem;
+  border-radius: 3rem;
+  background-color: #e4e4e4;
+  div {
+    background-color: #f3701a;
+    border-radius: 3rem;
+    width: ${(props) => props.percentage + "%"};
+    height: 100%;
+  }
+  
+`;
+const Weak = styled.div`
+  width: 10rem;
+  height: 1.8rem;
+  border-radius: 3rem;
+  background-color: #e4e4e4;
+
+  div {
+    background-color: #f3701a;
+    border-radius: 3rem;
+    width: ${(props) => props.percentage + "%"};
+    height: 100%;
+  }
+`;
+
+const Happinesslevel = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 2.5rem;
+  height: 2.5rem;
+  img {
+    margin: 0.1rem;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  
+`;
+
+ReactDOM.render(<Pokemon />, document.getElementById("root"));
+export default Pokemon
